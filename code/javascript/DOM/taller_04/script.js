c63a7ea27dadd8d86b16aa67622b592928469777
jalm0911 <jloorm4@gmail.com>
2023-06-22 20:09:51 -0500

    df

--- code/javascript/DOM/taller_04/script.js
+++ code/javascript/DOM/taller_04/script.js
@@ -2,7 +2,7 @@ function cargar_elemento() {
     var container = document.getElementById('container')
     var mensaje = document.getElementById('mensaje')
     var h1 = document.createElement('h1')
-    var texto = document.createTextNode(mensaje.value)
+    var texto = document.createTextNode(mensaje)
     h1.appendChild(texto)
     container.appendChild(h1)
 }
---
+++ code/javascript/DOM/taller_06/script.js
@@ -0,0 +1,9 @@
+function seleccionar_imagen() {
+    var container = document.getElementById('container')
+    container.add('vision')
+}
+
+function eliminar_imagen() {
+    var container = document.getElementById('container')
+    container.remove('vision')
+}
