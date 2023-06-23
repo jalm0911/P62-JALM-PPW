13c717589ec02af899156b16c1952f8a355bab91
jalm0911 <jloorm4@gmail.com>
2023-06-22 19:06:18 -0500

    DOM

---
+++ code/javascript/DOM/taller_03/script.js
@@ -0,0 +1,12 @@
+function cargar_elemento() {
+    var container = document.getElementById('container')
+    var h1 = document.createElement('h1')
+    var texto = document.createTextNode('Jordy Loor')
+    h1.appendChild(texto)
+    container.appendChild(h1)
+}
+
+function eliminar_elemento() {
+    var container = document.getElementById('container')
+    container.removeChild(container.lastElementChild)
+}
