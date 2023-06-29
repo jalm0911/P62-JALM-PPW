b4060f07f45e6691d192ed92dbd2c61edf92c3ee
jalm0911 <jloorm4@gmail.com>
2023-06-28 20:56:22 -0500

    DOM - Events

--- code/javascript/DOM/taller_07/script.js
+++ code/javascript/DOM/taller_07/script.js
@@ -1,2 +1,16 @@
 var div_cajita= document.getElementById('cajita')
-div_cajita.addEventListener()
+var es_presionado= false
+
+div_cajita.addEventListener('mousedown', function(valor){
+    es_presionado= true
+    console.log(es_presionado)
+}, true)
+
+div_cajita.addEventListener('mouseup', function(valor){
+    es_presionado= false
+    console.log(es_presionado)
+}, true)
+
+div_cajita.addEventListener('mousemove', function(valor){
+    console.log('[mousemove]')
+}, true)
