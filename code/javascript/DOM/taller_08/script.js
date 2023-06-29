e5e5b594cc1d0213bb804a4b499db457a110fc7f
jalm0911 <jloorm4@gmail.com>
2023-06-28 21:42:05 -0500

    sdvvdg

---
+++ code/javascript/DOM/taller_08/script.js
@@ -0,0 +1,9 @@
+var div_cajita= document.getElementById('cajita')
+setTimeout(()=> {
+    div_cajita.style.left= (div_cajita.style.left +10) + 'px'
+
+},2000);
+
+setTimeout(()=> {
+    div_cajita.style.left= (div_cajita.style.left +10) + 'px'
+},6000);
