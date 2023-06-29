c409504c176d3012b74a58a77674730067ab2cef
jalm0911 <jloorm4@gmail.com>
2023-06-28 21:18:54 -0500

    actual

--- code/javascript/DOM/taller_07/script.js
+++ code/javascript/DOM/taller_07/script.js
@@ -1,16 +1,27 @@
 var div_cajita= document.getElementById('cajita')
 var es_presionado= false
+var offset= [0, 0]
 
 div_cajita.addEventListener('mousedown', function(valor){
     es_presionado= true
+
+    offset= [div_cajita.offsetLeft - valor.clientX, div_cajita.offsetTop - valor.clientY]
+
     console.log(es_presionado)
 }, true)
 
 div_cajita.addEventListener('mouseup', function(valor){
     es_presionado= false
+
     console.log(es_presionado)
 }, true)
 
 div_cajita.addEventListener('mousemove', function(valor){
+    valor.preventDefault()
+    if(es_presionado) {
+        div_cajita.style.top= (valor.clientX + offset[0]) + 'px'
+        div_cajita.style.left= (valor.clientY + offset[1]) + 'px'
+    }
+    
     console.log('[mousemove]')
 }, true)
